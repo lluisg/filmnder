40aab85d156c0c493350f9385537e207f31c4b85
Lluis Guardia Fernandez <lluis.guardia.f@gmail.com>
2020-10-29 01:23:25 +0100

    UI plus some options

--- public/main.js
+++ public/main.js
@@ -2,6 +2,7 @@ var queryString = decodeURIComponent(window.location.search);
 queryString = queryString.substring(1);
 var user = queryString.split("=")[1];
 console.log('User: '+user);
+document.getElementById('username').innerHTML = user;
 
 
 // change font size whether the text is larger or not
@@ -13,3 +14,48 @@ console.log('User: '+user);
 // large
 // x-large
 // xx-large
+
+function filmLiked(){
+  console.log('Liked')
+}
+function filmDisliked(){
+  console.log('Disliked')
+}
+function filmSeen(){
+  console.log('Seen')
+}
+function filmRepeat(){
+  console.log('Repeat')
+}
+
+function showMatches(){
+  console.log('List of matches')
+  window.location.href='/matches.html?user='+user;
+}
+function showAlone(){
+  console.log('List of alone')
+  window.location.href='/alone.html?user='+user;
+}
+function changeValoration(){
+  console.log('Change valoration')
+  window.location.href='/valorations.html?user='+user;
+}
+
+
+function optionsButtonON(){
+  console.log('button on');
+  document.getElementById('options_buttons').style.opacity = "1";
+  document.getElementById('optionsON').style.opacity = "1";
+  document.getElementById('optionsON').style.zIndex = "2";
+  document.getElementById('optionsOFF').style.opacity = "0";
+  document.getElementById('optionsOFF').style.zIndex = "1";
+}
+
+function optionsButtonOFF(){
+  console.log('button off');
+  document.getElementById('options_buttons').style.opacity = "0";
+  document.getElementById('optionsON').style.opacity = "0";
+  document.getElementById('optionsON').style.zIndex = "1";
+  document.getElementById('optionsOFF').style.opacity = "1";
+  document.getElementById('optionsOFF').style.zIndex = "2";
+}
